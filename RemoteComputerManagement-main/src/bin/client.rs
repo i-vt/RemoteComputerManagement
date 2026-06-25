6b6f2b882afa895a472baa0a5291d592984dce48
user <user@hostname.attlocal.net>
2026-06-25 15:07:48 -0400

    Updated README.md

--- RemoteComputerManagement-main/src/bin/client.rs
+++
@@ -1,8 +0,0 @@
-// src/bin/client.rs 
-#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
-use rcm::agent;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    agent::run().await
-}
